c07af29d11b15c9297b44e792186289730a4342f
Bartlomiej Stepien <masafion@gmail.com>
2022-04-26 21:04:00 +0200

    bug fixed

--- src/main/java/pl/inteca/family/model/dtos/FamilyMemberDto.java
+++ src/main/java/pl/inteca/family/model/dtos/FamilyMemberDto.java
@@ -2,11 +2,13 @@ package pl.inteca.family.model.dtos;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class FamilyMemberDto {
 
     private Long id;
